189b846c99aee23d89e0d8ab0e50f0a378fba82c
Chidi Onyekwere <michaelpet40@gmail.com>
2025-04-22 13:23:21 +0100

    Fix lucide-react dependency issue in Vite config

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import LoginPage from '../views/LoginPage.vue'
-import SignupPage from '../views/SignupPage.vue'
+import SignUpPage from '../views/SignUpPage.vue'
 import ProfilePage from '../views/ProfilePage.vue'
 import CreatePage from '../views/CreatePage.vue'
 import PostDetailPage from '../views/PostDetailPage.vue'
@@ -10,7 +10,7 @@ import Posts from '../views/Posts.vue'
 const routes = [
   { path: '/', name: 'HomePage', component: HomePage },
   { path: '/LoginPage', name: 'LoginPage', component: LoginPage },
-  { path: '/SignupPage', name: 'SignupPage', component: SignupPage },
+  { path: '/SignUpPage', name: 'SignUpPage', component: SignUpPage },
   { path: '/ProfilePage', name: 'ProfilePage', component: ProfilePage },
   { path: '/CreatePage', name: 'CreatPage', component: CreatePage },
   { path: '/PostDetailPage', name: 'PostDetailPage', component: PostDetailPage },
